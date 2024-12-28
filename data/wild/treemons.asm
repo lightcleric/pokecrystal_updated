0d2a4a8cf48e186ae19425b859829689d04f1277
lightcleric <73899260+lightcleric@users.noreply.github.com>
2024-12-27 22:00:05 -0500

    fish grass

--- data/wild/treemons.asm
+++ data/wild/treemons.asm
@@ -19,43 +19,44 @@ TreeMons:
 TreeMonSet_City:
 TreeMonSet_Canyon:
 ; common
-	db 50, SPEAROW,    10
-	db 15, SPEAROW,    10
-	db 15, SPEAROW,    10
-	db 10, AIPOM,      10
-	db  5, AIPOM,      10
-	db  5, AIPOM,      10
+	db 50, PIKIPEK,    10
+	db 15, PIKIPEK,    10
+	db 15, PIKIPEK,    10
+	db 10, PIKIPEK,      10
+	db  5, PIKIPEK,      10
+	db  5, PIKIPEK,      10
 	db -1
 ; rare
-	db 50, SPEAROW,    10
+	db 50, PIKIPEK,    10
 	db 15, HERACROSS,  10
 	db 15, HERACROSS,  10
-	db 10, AIPOM,      10
-	db  5, AIPOM,      10
-	db  5, AIPOM,      10
+	db 10, PIKIPEK,      10
+	db  5, PIKIPEK,      10
+	db  5, PIKIPEK,      10
 	db -1
 
 TreeMonSet_Town:
 ; common
-	db 50, SPEAROW,    10
-	db 15, EKANS,      10
-	db 15, SPEAROW,    10
-	db 10, AIPOM,      10
-	db  5, AIPOM,      10
-	db  5, AIPOM,      10
+	db 50, PIKIPEK,    10
+	db 15, KOFFING,      10
+	db 15, PIKIPEK,    10
+	db 10, PIKIPEK,      10
+	db  5, PIKIPEK,      10
+	db  5, PIKIPEK,      10
 	db -1
 ; rare
 	db 50, SPEAROW,    10
+	db 50, PIKIPEK,    10
 	db 15, HERACROSS,  10
 	db 15, HERACROSS,  10
-	db 10, AIPOM,      10
-	db  5, AIPOM,      10
-	db  5, AIPOM,      10
+	db 10, PIKIPEK,      10
+	db  5, PIKIPEK,      10
+	db  5, PIKIPEK,      10
 	db -1
 
 TreeMonSet_Route:
 ; common
-	db 50, HOOTHOOT,   10
+	db 50, PIKIPEK,   10
 	db 15, SPINARAK,   10
 	db 15, LEDYBA,     10
 	db 10, EXEGGCUTE,  10
@@ -63,7 +64,7 @@ TreeMonSet_Route:
 	db  5, EXEGGCUTE,  10
 	db -1
 ; rare
-	db 50, HOOTHOOT,   10
+	db 50, PIKIPEK,   10
 	db 15, PINECO,     10
 	db 15, PINECO,     10
 	db 10, EXEGGCUTE,  10
@@ -73,15 +74,15 @@ TreeMonSet_Route:
 
 TreeMonSet_Kanto:
 ; common
-	db 50, HOOTHOOT,   10
-	db 15, EKANS,      10
-	db 15, HOOTHOOT,   10
+	db 50, PIKIPEK,   10
+	db 15, KOFFING,      10
+	db 15, PIKIPEK,   10
 	db 10, EXEGGCUTE,  10
 	db  5, EXEGGCUTE,  10
 	db  5, EXEGGCUTE,  10
 	db -1
 ; rare
-	db 50, HOOTHOOT,   10
+	db 50, PIKIPEK,   10
 	db 15, PINECO,     10
 	db 15, PINECO,     10
 	db 10, EXEGGCUTE,  10
@@ -91,15 +92,15 @@ TreeMonSet_Kanto:
 
 TreeMonSet_Lake:
 ; common
-	db 50, HOOTHOOT,   10
+	db 50, PIKIPEK,   10
 	db 15, VENONAT,    10
-	db 15, HOOTHOOT,   10
+	db 15, PIKIPEK,   10
 	db 10, EXEGGCUTE,  10
 	db  5, EXEGGCUTE,  10
 	db  5, EXEGGCUTE,  10
 	db -1
 ; rare
-	db 50, HOOTHOOT,   10
+	db 50, PIKIPEK,   10
 	db 15, PINECO,     10
 	db 15, PINECO,     10
 	db 10, EXEGGCUTE,  10
@@ -109,23 +110,23 @@ TreeMonSet_Lake:
 
 TreeMonSet_Forest:
 ; common
-	db 50, HOOTHOOT,   10
+	db 50, PIKIPEK,   10
 	db 15, PINECO,     10
 	db 15, PINECO,     10
-	db 10, NOCTOWL,    10
-	db  5, BUTTERFREE, 10
-	db  5, BEEDRILL,   10
+	db 10, TRUMBEAK,    10
+	db  5, LEDIAN, 10
+	db  5, LEDIAN,   10
 	db -1
 ; rare
-	db 50, HOOTHOOT,   10
-	db 15, CATERPIE,   10
-	db 15, WEEDLE,     10
-	db 10, HOOTHOOT,   10
-	db  5, METAPOD,    10
-	db  5, KAKUNA,     10
+	db 50, PIKIPEK,   10
+	db 15, LEDYBA,   10
+	db 15, LEDYBA,     10
+	db 10, PIKIPEK,   10
+	db  5, LEDYBA,    10
+	db  5, LEDYBA,     10
 	db -1
 
 TreeMonSet_Rock:
-	db 90, KRABBY,     15
+	db 90, WOOPER,     15
 	db 10, SHUCKLE,    15
 	db -1
